cc92151de11f0759b38a62a0000647a43fbf0f34
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-12-29 18:07:11 +0100

    Initial dist subtree commit

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-  verbose: true,
-  modulePathIgnorePatterns: [
-    '<rootDir>/dist/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/cypress/'
-  ]
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Accordion from './components/accordion'
-import './index.scss'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const dl = document.querySelector('dl')
-  const accordion = new Accordion(dl)
-  accordion.init()
-})
